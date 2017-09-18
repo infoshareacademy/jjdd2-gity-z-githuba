47311fcec09dfa281bc5b2876fd53c69215859e3
ArturDNet <artur.dabrowski.net@gmail.com>
2017-09-18 10:06:08 +0200

    1.0

--- src/pl/infoshareacademy/mail/ContactFinder.java
+++ src/pl/infoshareacademy/mail/ContactFinder.java
@@ -12,7 +12,6 @@ public class ContactFinder {
         Set<String> eFound = new HashSet<String>();
 
         for (Email e : list) {
-            e.getMessage();
             String splitterResult[] = e.getMessage().split("\\s");
             for (int i = 0; i < splitterResult.length; i++) {
                 if (splitterResult[i].matches("\\w+@\\w+\\.\\w+")) {
@@ -30,7 +29,6 @@ public class ContactFinder {
         List<Email> list = finder.getEmailList();
         Set<String> pFound = new HashSet<String>();
         for (Email e : list) {
-            e.getMessage();
             String splitterResult[] = e.getMessage().split("\\s");
             for (int i = 0; i < splitterResult.length; i++) {
                 if (splitterResult[i].matches("([\\d\\s+\\-)(])+?")
@@ -48,7 +46,6 @@ public class ContactFinder {
         List<Email> list = finder.getEmailList();
         Set<String> wFound = new HashSet<String>();
         for (Email e : list) {
-            e.getMessage();
             String splitterResult[] = e.getMessage().split("\\s");
             for (int i = 0; i < splitterResult.length; i++) {
                 if (splitterResult[i].matches("(http).+")
@@ -62,68 +59,4 @@ public class ContactFinder {
         }
     }
 }
-   /* public static void FindMail(Finder finder) {
-
-        String messageText;
-        List<Email> list = finder.getEmailList();
-        for (Email e : list) {
-            messageText = e.getDescription();
-            BufferedReader br = new BufferedReader(new StringReader(messageText));
-            try {
-                String messages = br.readLine();
-                while (messages != null) {
-                    splitter(messages);
-                    messages = br.readLine();
-                    System.out.println(messages);
-                }
-                br.close();
-            } catch (IOException error) {
-                error.printStackTrace();
-            }
-            System.out.println(messageText);
-        }
-
-    }*/
-
-   /* public static void splitter(java.lang.String string){
-        java.lang.String splitterResult[] = string.split("\\s");
-        for (int i = 0; i < splitterResult.length; i++){
-            if (splitterResult[i].matches("\\w+@\\w+\\.\\w+")
-                    || splitterResult[i].matches("([\\d\\s+\\-)(])+?")
-                    && splitterResult[i].length() >= 7) {
-                System.out.println("Email contact found: " + splitterResult[i]);
-            }
-        }
-    }
-    //TODO Wyrzuca listę stringów z emailami lub telefonami do wyboru
-
-    public static class findPhone {
-        File mboxfile;
-        Scanner sc;
-        ArrayList<String> maillist = new ArrayList();
-        public findPhone(String FilePath) {
-            mboxfile = new File(FilePath);
-        }
-        public void message() throws FileNotFoundException {
-            sc = new Scanner(mboxfile);
-            sc.useDelimiter("\\n{3}");
-            while (sc.hasNext()) {
-                maillist.add(sc.next());
-                for (int i = 0; i < maillist.size(); i++) {
-                    if (maillist.toString().matches("[\\d+\\s\\-)(]+?")){
-                        System.out.println("Phone found: " + maillist);
-                    }
-                }
-            }
-        }
-    }*/
-    //public static void notSplitted(String string){
-       // String lineByLineText[] = string.split("$");
-        //for (int i = 0; i < lineByLineText.length; i++){
-          //  if (lineByLineText[i].matches("[\\d+\\s\\-)(]+?")){
-             //   System.out.println("Email contact found: " + lineByLineText[i]);
-
-       // }
-  //  }
-//}
-
+  
