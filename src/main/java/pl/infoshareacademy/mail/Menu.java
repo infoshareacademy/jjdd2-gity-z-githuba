ee24d4b938e0c4752277a8ecd0f729c149d6a5d5
Artur <artur.dabrowski.net@gmail.com>
2017-09-21 17:51:23 +0200

    Hotfix jd2g 33 (#12)
    * dodanie paczek i zmiana struktury
    
    * hotfix#2

--- src/main/java/pl/infoshareacademy/mail/Menu.java
+++ src/main/java/pl/infoshareacademy/mail/Menu.java
@@ -3,39 +3,22 @@ import java.util.ArrayList;
 
 public class Menu {
     public static void PrintMenu(){
-        System.out.println("Welcome @User! How i can help you?");
+        System.out.println("Welcome @User! How can I help you?");
         System.out.println("");
-        System.out.format("%-60s - AVALIBLE%n","1. What keywords should i use to find mine email?");
-        System.out.format("%-60s - AVALIBLE%n","2. Analyze contact data from mine e-mail");
-        System.out.format("%-60s - UNAVALIBLE%n","3. Show me mine top 10 customer's");
-        System.out.format("%-60s - AVALIBLE%n","4. Filter mine contact's (Single .mbox file)");
-        System.out.format("%-60s - UNAVALIBLE%n","5. Filter mine contact's (Multiple .mbox file)");
+        System.out.format("%-60s - AVALIBLE%n","1. What keywords should I use to find my email?");
+        System.out.format("%-60s - AVALIBLE%n","2. Analyse contact data from my email");
+        System.out.format("%-60s - UNAVALIBLE%n","3. Show me my top 10 customers");
+        System.out.format("%-60s - AVALIBLE%n","4. Filter my contacts (Single .mbox file)");
+        System.out.format("%-60s - UNAVALIBLE%n","5. Filter my contacts (Multiple .mbox file)");
         System.out.format("%-60s - UNAVALIBLE%n","6. Show me abandoned customers");
         System.out.println();
         System.out.println("0. Quit");
     } // Function that prints menu on the screen
 
-    public static void PrintMenuKeywords() {
+     public static void PrintMenuAnalyze(){
         Messenger msg = new Messenger();
         msg.warningErrorReport("Function not done yet!");
     }
 
-    public static void PrintMenuAnalyze(){
-        Messenger msg = new Messenger();
-        msg.warningErrorReport("Function not done yet!");
-    }
 
-    public static void filterMaches(boolean mail, boolean phone, ArrayList<String> mailContent){
-        for (int i = 0; i < mailContent.size(); i++){
-            if (mailContent.get(i).contains("\\w+@\\w+\\.\\w+")) {
-                System.out.println("Email address found: " + mailContent.get(i));
-                String[] splitResult = mailContent.get(i).split("\\s");
-                for (int j = 0; j < splitResult.length; j++){
-                }
-            } else if (mailContent.get(i).matches("([\\d\\s+\\-)(])+?")
-                    && mailContent.get(i).length() >= 7){
-                System.out.println("Mobile number found: " + mailContent.get(i));
-            }
-        }
-    }
 }
