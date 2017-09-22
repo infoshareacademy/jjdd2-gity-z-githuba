a8bd81d89555038c87752e0d13010193b418d9fb
Paweł Maryszczuk <Pawi133@gmail.com>
2017-09-22 16:59:24 +0200

    Fixed save property value

--- src/main/java/pl/infoshareacademy/config/ConfigureSave.java
+++ src/main/java/pl/infoshareacademy/config/ConfigureSave.java
@@ -1,23 +1,24 @@
-package pl.infoshareacademy.mail;
-
+package pl.infoshareacademy.config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.omg.CORBA.LongLongSeqHelper;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Properties;
 
 public class ConfigureSave {
-    private static final Logger logger = LogManager.getLogger(ContactFinder.class.getName());
+    private static final Logger logger = LogManager.getLogger(ConfigureSave.class.getName());
     private static String configPath = "jjdd2-gity-z-githuba/src/main/resources/config.properties";
 
     public static void main(String[] args) {
-        Properties prop = new Properties();
+        setSettingsValue("LOL");
+     /*   Properties prop = new Properties();
         OutputStream output = null;
         try {
             output = new FileOutputStream(configPath);
-            prop.setProperty("Database", "Localhost");
             prop.setProperty("dbuser", "admin");
             prop.store(output, null);
         } catch (IOException e) {
@@ -30,6 +31,27 @@ public class ConfigureSave {
                     logger.fatal("Output == null! Closing output stream.");
                 }
             }
+        }*/
+
+    }
+
+    public static void setSettingsValue(String key) {
+        Properties prop = new Properties();
+        OutputStream output = null;
+        try {
+            FileInputStream in = new FileInputStream(configPath);
+            prop.load(in);
+            in.close();
+        } catch (IOException e) {
+            logger.fatal("File not found! {}", configPath);
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(configPath);
+            prop.setProperty("pedal", "cwel");
+            prop.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            logger.fatal("File not found! {}", configPath);
         }
     }
 }
