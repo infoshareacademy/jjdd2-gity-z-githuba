c9f8d93f412ee9dc0eb5163350ffc6df8c8d5d59
walpio <doksmal@gmail.com>
2017-09-24 22:47:53 +0200

    merge with develop and added loggs

--- src/main/java/pl/infoshareacademy/mail/KeyWords.java
+++ src/main/java/pl/infoshareacademy/mail/KeyWords.java
@@ -1,8 +1,14 @@
 package pl.infoshareacademy.mail;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.security.Key;
 import java.util.Scanner;
 
 public class KeyWords {
+
+    private static final Logger logger = LogManager.getLogger(KeyWords.class.getName());
 
     private Scanner scanner = new Scanner(System.in);
 
@@ -19,16 +25,19 @@ public class KeyWords {
             String wyborUzytkownika = getUserInput();
 
             while (!(wyborUzytkownika.equals("y") || wyborUzytkownika.equals("n"))) {
+                logger.warn("Question #" + (i + 1) + ". Input mismatch! Expected Y or N got other value!");
                 System.out.println("Please enter (Y)es or (N)o");
                 wyborUzytkownika = scanner.nextLine().toLowerCase();
             }
             switch (wyborUzytkownika) {
                 case "y":
+                    logger.info("User answered Yes to the question #" + (i + 1));
                     System.out.println();
                     System.out.println("These keywords might be helpful for you:");
                     System.out.println(question.getListOfAnswersIfYes().get(i));
                     break;
                 case "n":
+                    logger.info("User answered No to the question #" + (i + 1));
                     System.out.println();
                     System.out.println("These keywords might be helpful for you:");
                     System.out.println(question.getListOfAnswersIfNo().get(i));
