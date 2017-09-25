10b1f241ce6db85a9f02d678b6c3d440fa688118
Paweł Maryszczuk <Pawi133@gmail.com>
2017-09-25 15:32:48 +0200

    Fixed Logger level, Fixed Logger getClass
    Usage:
    ConfigureSave.setProperty
    ConfigureLoad.getPropertyValue

--- src/main/java/pl/infoshareacademy/config/ConfigureLoad.java
+++ src/main/java/pl/infoshareacademy/config/ConfigureLoad.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigureLoad {
-    private static final Logger logger = LogManager.getLogger(ConfigureSave.class.getName());
-    private static String configPath = "src/main/resources/config.properties";
+    private static final Logger logger = LogManager.getLogger(ConfigureLoad.class.getName());
+    private static String configPath = "jjdd2-gity-z-githuba/src/main/resources/config.properties";
     private Properties prop = new Properties();
 
     public Properties loadConfigurationFile() {
@@ -17,7 +17,7 @@ public class ConfigureLoad {
             prop.load(in);
             in.close();
         } catch (IOException e) {
-            logger.fatal("Failed while loading config file! File path was: {}", configPath);
+            logger.error("Failed while loading config file! File path was: {}", configPath);
         }
         this.prop = prop;
         return prop;
@@ -27,14 +27,5 @@ public class ConfigureLoad {
         return prop.getProperty(key);
     }
 
-    public static void main(String[] args) {
-        ConfigureLoad conf = new ConfigureLoad();
-        System.out.println("Hello World!");
-        conf.loadConfigurationFile();
-        System.out.println(conf.getConfigurationValue("pedal"));
-        System.out.println(conf.getConfigurationValue("country"));
-        System.out.println(conf.getConfigurationValue("dbuser"));
-
-    }
 }
 
--- src/main/java/pl/infoshareacademy/config/ConfigureSave.java
+++ src/main/java/pl/infoshareacademy/config/ConfigureSave.java
@@ -5,22 +5,20 @@ import org.apache.logging.log4j.Logger;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Properties;
 
 public class ConfigureSave {
     private static final Logger logger = LogManager.getLogger(ConfigureSave.class.getName());
-    private static String configPath = "src/main/resources/config.properties";
+    private static String configPath = "jjdd2-gity-z-githuba/src/main/resources/config.properties";
 
     public static void setSettingsValue(String key, String value) {
         Properties prop = new Properties();
-        OutputStream output = null;
         try {
             FileInputStream in = new FileInputStream(configPath);
             prop.load(in);
             in.close();
         } catch (IOException e) {
-            logger.fatal("File not found! {}", configPath);
+            logger.error("File not found! {}", configPath);
         }
         try {
             FileOutputStream out = new FileOutputStream(configPath);
@@ -28,7 +26,7 @@ public class ConfigureSave {
             prop.store(out, null);
             out.close();
         } catch (IOException e) {
-            logger.fatal("File not found! {}", configPath);
+            logger.error("File not found! {}", configPath);
         }
     }
 }
