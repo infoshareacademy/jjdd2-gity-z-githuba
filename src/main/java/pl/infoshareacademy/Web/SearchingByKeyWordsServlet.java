92a69aa0ec89215d702d435c68031759f10a2138
walpio <doksmal@gmail.com>
2017-10-08 16:31:49 +0200

    nowy servlet na słowa kluczowe; wersja testowa

--- src/main/java/pl/infoshareacademy/Web/SearchingByKeyWordsServlet.java
+++ src/main/java/pl/infoshareacademy/Web/SearchingByKeyWordsServlet.java
@@ -4,11 +4,13 @@ package pl.infoshareacademy.Web;
 import pl.infoshareacademy.mail.QuestionsAndAnswers;
 
 import javax.print.DocFlavor;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -50,19 +52,36 @@ public class SearchingByKeyWordsServlet extends HttpServlet {
         writer.println("<option value=\"No\">No</option>");
         writer.println("</select>");
         writer.println("<br><br>");
+        writer.println("</form>");
+        writer.println("<form method=\"get\" action=\"words\">");
         writer.println("<button type=\"submit\">Give me the list of keywords!</button>");
         writer.println("</form>");
         writer.println("</body>");
         writer.println("</html>");
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("text/html;charset=UTF-8");
+        HttpSession session = req.getSession();
+        String firstAnswer = req.getParameter("question1");
+        String secondAnswer = req.getParameter("question2");
+        String thirdAnswer = req.getParameter("question3");
+        session.setAttribute("firstAnswer", firstAnswer);
+        session.setAttribute("secondAnswer", secondAnswer);
+        session.setAttribute("thirdAnswer", thirdAnswer);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/words");
+        dispatcher.forward(req, resp);
+
+
+    }
+
+/*        resp.setContentType("text/html;charset=UTF-8");
 
         PrintWriter writer = resp.getWriter();
-//
+
         QuestionsAndAnswers question = new QuestionsAndAnswers();
 
         String firstAnswer = req.getParameter("question1");
@@ -104,5 +123,5 @@ public class SearchingByKeyWordsServlet extends HttpServlet {
                 break;
         }
 
-    }
+    }*/
 }
