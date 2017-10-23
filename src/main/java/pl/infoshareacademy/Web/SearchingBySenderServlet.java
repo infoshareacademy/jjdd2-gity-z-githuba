91c84ea80e977aa4a533ff8d8cb4eca2240982e5
Michał Richert <michalrichert@gmail.com>
2017-10-23 11:54:46 +0200

    Merge branch 'develop' into feature/JD2G-stats

--- src/main/java/pl/infoshareacademy/Web/SearchingBySenderServlet.java
+++ src/main/java/pl/infoshareacademy/Web/SearchingBySenderServlet.java
@@ -3,15 +3,15 @@ package pl.infoshareacademy.Web;
 import pl.infoshareacademy.mail.TempFilePath;
 
 import javax.inject.Inject;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet("/sender")
 public class SearchingBySenderServlet extends HttpServlet {
