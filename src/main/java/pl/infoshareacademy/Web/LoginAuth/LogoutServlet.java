98d4c910e4e983eff9ba665d5cae23b551a212f3
Michal Richert <michalrichert@gmail.com>
2017-11-05 19:35:44 +0100

    Fix redirect

--- src/main/java/pl/infoshareacademy/Web/LoginAuth/LogoutServlet.java
+++ src/main/java/pl/infoshareacademy/Web/LoginAuth/LogoutServlet.java
@@ -3,7 +3,6 @@ package pl.infoshareacademy.Web.LoginAuth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,11 +20,7 @@ public class LogoutServlet extends HttpServlet {
             logger.info("User has logged out!");
             request.getSession().invalidate();
         }
-//        response.sendRedirect("jsp/exit.jsp");
         request.getSession().invalidate();
-     /*   RequestDispatcher dispatcher = getServletContext()
-                .getRequestDispatcher("jsp/home.jsp");
-        dispatcher.forward(request, response);}*/
         request.getRequestDispatcher("jsp/exit.jsp").include(request, response);
     }
 }
