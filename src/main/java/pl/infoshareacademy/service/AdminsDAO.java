8a7826f57d171803451eac79d17481b7abf38d3a
SowaTheKing <Zqbeirrt!23>
2017-11-05 15:32:30 +0100

    User.dao and Admins.dao done

---
+++ src/main/java/pl/infoshareacademy/service/AdminsDAO.java
@@ -0,0 +1,26 @@
+package pl.infoshareacademy.service;
+
+import pl.infoshareacademy.model.Admins;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class AdminsDAO {
+    @PersistenceContext(unitName = "gity-db")
+    EntityManager em;
+
+    public boolean findAdminByToken(String token) {
+        List<Admins> result = em.createQuery(
+                "SELECT e FROM Admins e WHERE e.token LIKE :token")
+                .setParameter("token", token)
+                .setMaxResults(5)
+                .getResultList();
+        if (result.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
