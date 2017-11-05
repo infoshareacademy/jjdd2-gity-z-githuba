aa045dbcda737d980fcc8d0216e42b7ee11cfb41
Michal Richert <michalrichert@gmail.com>
2017-11-05 19:34:50 +0100

    works

--- src/main/java/pl/infoshareacademy/Web/LoginAuth/LogoutServlet.java
+++ src/main/java/pl/infoshareacademy/Web/LoginAuth/LogoutServlet.java
@@ -3,6 +3,7 @@ package pl.infoshareacademy.Web.LoginAuth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +21,11 @@ public class LogoutServlet extends HttpServlet {
             logger.info("User has logged out!");
             request.getSession().invalidate();
         }
-        response.sendRedirect("jsp/exit.jsp");
+//        response.sendRedirect("jsp/exit.jsp");
+        request.getSession().invalidate();
+     /*   RequestDispatcher dispatcher = getServletContext()
+                .getRequestDispatcher("jsp/home.jsp");
+        dispatcher.forward(request, response);}*/
+        request.getRequestDispatcher("jsp/exit.jsp").include(request, response);
     }
 }
